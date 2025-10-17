cec34d374bc7124eaaa9846240aa04a23e13fef3
Daan Hollands <daan.hollands@student.uhasselt.be>
2025-10-17 23:07:53 +0200

    Basic Edits for CachyOS

--- install/login/all.sh
+++ install/login/all.sh
@@ -1,4 +1,1 @@
-run_logged $OMARCHY_INSTALL/login/plymouth.sh
-run_logged $OMARCHY_INSTALL/login/limine-snapper.sh
 run_logged $OMARCHY_INSTALL/login/enable-mkinitcpio.sh
-run_logged $OMARCHY_INSTALL/login/alt-bootloaders.sh
--- install/post-install/all.sh
+++ install/post-install/all.sh
@@ -1,2 +1,1 @@
-run_logged $OMARCHY_INSTALL/post-install/pacman.sh
 source $OMARCHY_INSTALL/post-install/finished.sh
